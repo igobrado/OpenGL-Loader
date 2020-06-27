db61ad0f30b25c0c1c93e742fa4709f368592c56
Obradovic Igor <extern.igor.obradovic@porsche.de>
2020-06-27 19:14:18 +0200

    Add mesh files

---
+++ include/Mesh.cxx
@@ -0,0 +1,5 @@
+//
+// Created by iob on 27. 06. 2020..
+//
+
+#include "Mesh.hxx"
---
+++ include/Mesh.hxx
@@ -0,0 +1,10 @@
+#ifndef OPENGL_PBR_MESH_HXX
+#define OPENGL_PBR_MESH_HXX
+
+
+class Mesh {
+
+};
+
+
+#endif //OPENGL_PBR_MESH_HXX
