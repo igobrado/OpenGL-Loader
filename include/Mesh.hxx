cbdd5619d12bcfd95e0be4680b1b108860c6e67f
Obradovic Igor <extern.igor.obradovic@porsche.de>
2020-06-29 15:42:00 +0200

    Use RAII for bind-unbind buffers

--- include/Mesh.hxx
+++ include/Mesh.hxx
@@ -2,6 +2,7 @@
 #define OPENGL_PBR_MESH_HXX
 
 #include <GL/glew.h>
+
 #include <vector>
 
 class Mesh
@@ -9,16 +10,118 @@ class Mesh
 public:
     Mesh();
     ~Mesh();
-    void createMesh(std::vector<float>& vertices, std::vector<unsigned int>& indices);
+    void createMesh(std::vector<float>& vertices, std::vector<std::uint32_t>& indices);
     void renderMesh();
     void clearMesh();
-private:
-    unsigned int mVao;
-    unsigned int mVbo;
-    unsigned int mIbo;
 
-    std::size_t  mIndexCount;
+private:
+    std::uint32_t mVao;
+    std::uint32_t mVbo;
+    std::uint32_t mIbo;
+
+    std::size_t mIndexCount;
+    enum class DrawType
+    {
+        GL_STATIC  = GL_STATIC_DRAW,
+        GL_DYNAMIC = GL_DYNAMIC_DRAW
+    };
+    struct BindUnbindVAOCreateMesh
+    {
+        explicit BindUnbindVAOCreateMesh(std::uint32_t& vao)
+        {
+            glGenVertexArrays(1, &vao);
+            glBindVertexArray(vao);
+        }
+
+        ~BindUnbindVAOCreateMesh()
+        {
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        }
+
+    private:
+    };
+
+    struct BindUnbindIBOCreateMesh
+    {
+        BindUnbindIBOCreateMesh(unsigned int& ibo, std::vector<std::uint32_t>& indices, DrawType drawType)
+        {
+            glGenBuffers(1, &ibo);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+            glBufferData(
+                    GL_ELEMENT_ARRAY_BUFFER,
+                    indices.size() * sizeof(indices[0]),
+                    indices.data(),
+                    drawType == DrawType::GL_STATIC ? GL_STATIC_DRAW : GL_DYNAMIC_DRAW);
+        }
+
+        ~BindUnbindIBOCreateMesh()
+        {
+            // UNBIND IBO
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        }
+    };
+
+    struct BindUnbindVBOCreateMesh
+    {
+        BindUnbindVBOCreateMesh(std::uint32_t& vbo, std::vector<float>& vertices, DrawType drawType)
+        {
+            glGenBuffers(1, &vbo);
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glBufferData(
+                    GL_ARRAY_BUFFER,
+                    vertices.size() * sizeof(vertices[0]),
+                    vertices.data(),
+                    drawType == DrawType::GL_STATIC ? GL_STATIC_DRAW : GL_DYNAMIC_DRAW);
+        }
+
+        ~BindUnbindVBOCreateMesh()
+        {
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+        }
+    };
+
+    struct BindUnbindVertexAttribPointerCreateMesh
+    {
+        BindUnbindVertexAttribPointerCreateMesh(
+                unsigned int index,
+                int          size,
+                GLenum       type,
+                bool         normalized,
+                int          stride,
+                const void*  ptr)
+        {
+            glVertexAttribPointer(index, size, type, false, stride, ptr);
+        }
+
+        ~BindUnbindVertexAttribPointerCreateMesh()
+        {
+            glEnableVertexAttribArray(0);
+        }
+    };
+    struct BindVAORenderMesh
+    {
+        explicit BindVAORenderMesh(std::uint32_t& vao)
+        {
+            glBindVertexArray(vao);
+        }
+
+        ~BindVAORenderMesh()
+        {
+            glBindVertexArray(0);
+        }
+    };
+
+    struct BindIBORenderMesh
+    {
+        explicit BindIBORenderMesh(std::uint32_t& ibo)
+        {
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        }
+        ~BindIBORenderMesh()
+        {
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        }
+    };
 };
 
-
-#endif //OPENGL_PBR_MESH_HXX
+#endif  // OPENGL_PBR_MESH_HXX
--- src/Mesh.cxx
+++ src/Mesh.cxx
@@ -13,45 +13,30 @@ Mesh::~Mesh()
     clearMesh();
 }
 
-void Mesh::createMesh(std::vector<float>& vertices, std::vector<unsigned int>& indices)
+void Mesh::createMesh(std::vector<float>& vertices, std::vector<std::uint32_t>& indices)
 {
     mIndexCount = indices.size();
+    // BIND VAO - BINDS FIRST, UNBIND FOURTH
+    BindUnbindVAOCreateMesh vaoBind{ mVao };
 
-    // BIND VAO
-    glGenVertexArrays(1, &mVao);
-    glBindVertexArray(mVao);
+    // BIND IBO - BINDS SECOND, UNBIND THIRD
+    BindUnbindIBOCreateMesh iboBind{ mIbo, indices, DrawType::GL_STATIC };
 
-    // BIND IBO
-    glGenBuffers(1, &mIbo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mIbo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(indices[0]), indices.data(), GL_STATIC_DRAW);
+    // BIND VBO - BINDS THIRD, UNBIND SECOND
+    BindUnbindVBOCreateMesh vboBind{ mVbo, vertices, DrawType::GL_STATIC };
 
-    // BIND VBO
-    glGenBuffers(1, &mVbo);
-    glBindBuffer(GL_ARRAY_BUFFER, mVbo);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices[0]), vertices.data(), GL_STATIC_DRAW);
-
-    // SET ATTRIB POINTER
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(0);
-
-    // UNBIND VBO
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // UNBIND IBO
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    // UNBIND VAO
-    glBindVertexArray(0);
+    // BIND Vertex attrib pointer, BIND FOURTH, UNBIND FIRST
+    BindUnbindVertexAttribPointerCreateMesh bindVertexAttribPointer{ 0, 3, GL_FLOAT, false, 0, 0 };
 }
 
 void Mesh::renderMesh()
 {
-    glBindVertexArray(mVao);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mIbo);
+    // BIND VAO for rendering mesh - BOUND FIRST, UNBOUND LAST
+    BindVAORenderMesh bindVaoRenderMesh{ mVao };
+    // BIND IBO for rendering mesh - BOUND SECOND, UNBOUND FIRST
+    BindIBORenderMesh bindIboRenderMesh{ mIbo };
+
     glDrawElements(GL_TRIANGLES, mIndexCount, GL_UNSIGNED_INT, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
 }
 
 void Mesh::clearMesh()
